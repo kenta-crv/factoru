33309ba3bcc01c1db6f82c9e43d22c210ac479f5
kenta-crv <okuyamakenta@gmail.com>
2021-09-18 20:15:28 +0900

    columns fix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,11 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://shiokuritai.com"
+SitemapGenerator::Sitemap.default_host = "https://factoru.jp"
 
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
+add "/" , changefreq: 'daily', priority: 1.0
+
   add columns_path, :priority => 1.0, :changefreq => 'daily'
   Column.find_each do |column|
     add column_path(column), :lastmod => column.updated_at
