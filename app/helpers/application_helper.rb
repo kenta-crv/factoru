ad7e378d8f0886c91fbec96050fa916057f79a71
kenta-crv <okuyamakenta@gmail.com>
2021-10-14 14:26:10 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "栄養士専門の求人サイトなら",
-      title:"<%= yield(:title) | メディワーカー' %>",
-      description: "栄養士専門の求人サイトならメディワーカー。全国各地の求人を掲載し、求職者の方の最善の就職をサポートします。",
+      site: "工場の転職コラムなら",
+      title:"<%= yield(:title) | 工場転職のファクトル' %>",
+      description: "ファクトルでは、工場を専門にした転職コラム記事を投稿しております。",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       reverse: true,
