6b88757f0a5c39c4236536ffcd9771c6c9e1a50c
kenta-crv <okuyamakenta@gmail.com>
2021-10-21 14:10:16 +0900

    floara delete

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,4 +1,6 @@
 class Column < ApplicationRecord
+  DELETE_TEXT_1 = "Powered by"
+  DELETE_TEXT_2 = "Froala Editor"
   mount_uploader :file_1, ImagesUploader
   mount_uploader :file_2, ImagesUploader
   mount_uploader :file_3, ImagesUploader
@@ -16,4 +18,66 @@ class Column < ApplicationRecord
    description: description
  }
  end
+
+ def display_content_1
+   self.content_1.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_2
+   self.content_2.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_3
+   self.content_3.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_4
+   self.content_4.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_5
+   self.content_5.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_6
+   self.content_6.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_7
+   self.content_7.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_8
+   self.content_8.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_9
+   self.content_9.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_content_10
+   self.content_10.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_1
+   self.heading_1.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_2
+   self.heading_2.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_3
+   self.heading_3.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_4
+   self.heading_4.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_5
+   self.heading_5.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_6
+   self.heading_6.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_7
+   self.heading_7.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_8
+   self.heading_8.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_9
+   self.heading_9.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+ def display_heading_10
+   self.heading_10.delete(DELETE_TEXT_1).delete(DELETE_TEXT_2)
+ end
+
 end
